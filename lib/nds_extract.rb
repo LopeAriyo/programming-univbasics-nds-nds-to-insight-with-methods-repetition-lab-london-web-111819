c2217b61caf8395a7f1743e2bed46b8333aa81d5
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-06 13:59:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,10 +49,10 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 
-  i = 0 
-  running_total = 0 
+  i = 0
+  running_total = 0
 
-  
+
   directors_totals(source)
   list_of_directors(source).length
 # p directors_totals(directors_database)[director_data[:name]] - example of what you did in last session
